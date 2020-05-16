b3436eb32e2e3ad64c99d382c2f181fd0d3958bd
laiyam <laibyui@gmail.com>
2020-05-16 11:15:34 -0400

    fixed bugs, enable touch reset button, add color to winner

--- week04/tic-tac-toe.js
+++ week04/tic-tac-toe.js
@@ -7,14 +7,15 @@ var winSigns = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
 ];
 
+var gameNum = 1;
 var lastWin = p1;
 var gameActive = true;
 var gameType = "new"
 var p1, p2, playerSymbol = "O";
 
 //Player 1 selects symbol
-var getButton = document.getElementByID("button.id");
-getButton.addEventListener("touchend", selectSymbol());
+document.getElementByID("button.id").addEventListener("touchend", selectSymbol());
+document.getElementByID("resetButton").addEventListener("touchend", reset());
 
 //Declare the player's symbol
 function selectSymbol() {
@@ -29,7 +30,7 @@ function selectSymbol() {
             p1 = "O";
             p2 = "X";
         }
-        //document.getElementById("msg").innerHTML = "Player 2 is using '" + s + "' for the game.";
+ 
         document.getElementById("player1").value = p1;
         document.getElementById("player1").innerHTML = p1;
         document.getElementById("player2").value = p2;
@@ -44,7 +45,6 @@ function selectSymbol() {
 
 //Player function to mark the cell
 function markCell(id) {
-    //document.getElementById("msg").innerHTML = "LastWin: " + lastWin;
     if (gameActive == true) {
 
         if (document.getElementById(id).innerHTML == "") {
@@ -59,6 +59,7 @@ function markCell(id) {
         } else {
             playerSymbol = "O";
         }
+        document.getElementById("topMsg").innerHTML = "Game " + gameNum;
     }
 }
 
@@ -74,15 +75,18 @@ function mark(tdId, cellSymbol) {
 
 //Reset the game
 function reset() {
+    gameNum++;
+    document.getElementById("topMsg").innerHTML = "Game " + gameNum;
     for (i = 0; i < 10; i++) {
         cellIdx = "p" + i;
         cell[i] = "";
+        gameActive = true;
         document.getElementById(cellIdx).innerHTML = "";
         document.getElementById(cellIdx).style.color = "black";
-        //document.getElementById("msg").innerHTML = "RESET: " + gameActive;
-        gameActive = true;
-        document.getElementById("msg").innerHTML = "Game Reseted! The last loser starts the game first."
-    }
+        document.getElementById("msg").style.color = "black";
+        document.getElementById("msg").innerHTML = "Game Reseted! The last loser starts the game first.";
+    };
+    
 }
 
 //Check the Winner
@@ -92,9 +96,11 @@ function checkWinner(symbol) {
             cell[winSigns[i][1]] == symbol &&
             cell[winSigns[i][2]] == symbol) {
             if (symbol == document.getElementById("player1").innerHTML) {
+                document.getElementById("msg").style.color = "blue";
                 document.getElementById("msg").innerHTML = "Player 1 won the game!";
                 lastWin = "p1";
             } else {
+                document.getElementById("msg").style.color = "blue";
                 document.getElementById("msg").innerHTML = "Player 2 won the game!";
                 lastWin = "p2";
             }
