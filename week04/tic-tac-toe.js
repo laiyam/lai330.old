552189e62554850d5fa75809615a1efaa1e0e110
laiyam <laibyui@gmail.com>
2020-05-17 05:01:19 -0400

    Release Version 1

--- week04/tic-tac-toe.js
+++ week04/tic-tac-toe.js
@@ -8,6 +8,8 @@ var winSigns = [
 ];
 
 var gameNum = 1;
+var p1Wins = 0;
+var p2Wins = 0;
 var lastWin = p1;
 var gameActive = true;
 var gameType = "new"
@@ -15,7 +17,7 @@ var p1, p2, playerSymbol = "O";
 
 //Player 1 selects symbol
 document.getElementByID("button.id").addEventListener("touchend", selectSymbol());
-document.getElementById(id).addEventListener("onclick", mark(id, playerSymbol));
+//document.getElementById("p0").addEventListener("click", mark("p0", playerSymbol());
 document.getElementByID("resetButton").addEventListener("touchend", reset());
 
 //Declare the player's symbol
@@ -36,24 +38,38 @@ function selectSymbol() {
         document.getElementById("player1").innerHTML = p1;
         document.getElementById("player2").value = p2;
         document.getElementById("player2").innerHTML = p2;
-        playerSymbol = p1;
-
-        //Timeout for Symbol Selection in 3 seconds
-        //setTimeout(startMsg, 3000);
     }
+    playerSymbol = p1;
 }
+
 
 //Player function to mark the cell
 function markCell(id) {
     gameType = "old";
     if (gameActive == true) {
+         if (document.getElementById("p1Label").style.getPropertyValue("font-weight") == "900") {
+            document.getElementById("msg").innerHTML = "Turn: Player 2";
+        } else {
+            document.getElementById("msg").innerHTML = "Turn: Player 1";
+        }
+
         if (document.getElementById(id).innerHTML == "") {
+            if (document.getElementById("p1Label").style.getPropertyValue("font-weight") == "900") {
+            document.getElementById("p2Label").style.fontWeight = "900";
+            document.getElementById("player2").style.fontWeight = "600";
+            document.getElementById("p1Label").style.fontWeight = "normal";
+            document.getElementById("player1").style.fontWeight = "normal";
+        } else {
+            document.getElementById("p1Label").style.fontWeight = "900";
+            document.getElementById("player1").style.fontWeight = "600";
+            document.getElementById("p2Label").style.fontWeight = "normal";
+            document.getElementById("player2").style.fontWeight = "normal";
+        }
             document.getElementById(id).innerHTML = playerSymbol;
             mark(id, playerSymbol);
             checkWinner(playerSymbol);
         }
-
-        //swich player symbol
+        //Swich player symbol
         if (playerSymbol == "O") {
             playerSymbol = "X";
         } else {
@@ -72,6 +88,49 @@ function mark(tdId, cellSymbol) {
         document.getElementById(tdId).innerHTML = playerSymbol;
     }
 }
+    
+
+//Check the Winner
+function checkWinner(symbol) {
+    for (i in winSigns) {
+        if (cell[winSigns[i][0]] == symbol &&
+            cell[winSigns[i][1]] == symbol &&
+            cell[winSigns[i][2]] == symbol) {
+            if (symbol == document.getElementById("player1").innerHTML) {
+                ++p1Wins;
+                gameActive = false;
+                document.getElementById("msg").style.color = "red";
+                document.getElementById("p2Label").style.fontWeight = "900";
+                document.getElementById("player2").style.fontWeight = "600";
+                document.getElementById("msg").innerHTML = "Player 1 won the game!";
+                document.getElementById("p1Score").innerHTML = p1Wins + " WINS";
+                document.getElementById("p1Score").style.color = "red";
+                lastWin = "p1";
+            } else {
+                ++p2Wins;
+                gameActive = false;
+                document.getElementById("msg").style.color = "red";
+                document.getElementById("p1Label").style.fontWeight = "900";
+                document.getElementById("player1").style.fontWeight = "600";
+                document.getElementById("msg").innerHTML = "Player 2 won the game!";
+                document.getElementById("p2Score").innerHTML = p2Wins + " WINS";
+                document.getElementById("p2Score").style.color = "red";
+                lastWin = "p2";
+            }
+            //document.getElementById("msg").innerHTML = "Winner marks is: " + symbol;   //control
+
+            var winMarks = winSigns[i];
+            for (j in winMarks) {
+                inx = "p" + winMarks[j];
+                document.getElementById(inx).style.color = "red";
+            }
+            
+            //switchSymbol();
+        }
+    }
+    return playerSymbol;
+}
+
 
 //Reset the game
 function reset() {
@@ -85,49 +144,15 @@ function reset() {
         gameActive = true;
         document.getElementById(cellIdx).innerHTML = "";
         document.getElementById(cellIdx).style.color = "black";
-        document.getElementById("player1").style.color = "black";
-        document.getElementById("player2").style.color = "black";
-        document.getElementById("p1Label").style.color = "black";
-        document.getElementById("p2Label").style.color = "black";
+        document.getElementById("p1Score").style.color = "blue";
+        document.getElementById("p2Score").style.color = "blue";
         document.getElementById("msg").style.color = "black";
         if (lastWin == "p1") {
-            document.getElementById("msg").innerHTML = "Game Reseted!  Player 1 starts the new game first.";
+            document.getElementById("msg").innerHTML = "Player 2 starts the new game first.";
         } else {
-            document.getElementById("msg").innerHTML = "Game Reseted!  Player 2 starts the new game first.";
+            document.getElementById("msg").innerHTML = "Player 1 starts the new game first.";
         }
-        
+
     };
-    
-}
 
-//Check the Winner
-function checkWinner(symbol) {
-    for (i in winSigns) {
-        if (cell[winSigns[i][0]] == symbol &&
-            cell[winSigns[i][1]] == symbol &&
-            cell[winSigns[i][2]] == symbol) {
-            if (symbol == document.getElementById("player1").innerHTML) {
-                document.getElementById("msg").style.color = "red";
-                document.getElementById("player1").style.color = "red";
-                document.getElementById("p1Label").style.color = "red";
-                document.getElementById("msg").innerHTML = "Player 1 won the game!";
-                lastWin = "p1";
-            } else {
-                document.getElementById("msg").style.color = "red";
-                document.getElementById("player2").style.color = "red";
-                document.getElementById("p2Label").style.color = "red";
-                document.getElementById("msg").innerHTML = "Player 2 won the game!";
-                lastWin = "p2";
-            }
-            //document.getElementById("msg").innerHTML = "Winner marks is: " + symbol;   //control
-
-            var winMarks = winSigns[i];
-            for (j in winMarks) {
-                inx = "p" + winMarks[j];
-                document.getElementById(inx).style.color = "red";
-            }
-            gameActive = false;
-        }
-    }
-    return playerSymbol;
 }
