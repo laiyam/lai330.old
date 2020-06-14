e458959558883dbcedb28802d47d5f7b1afa0230
laiyam <laibyui@gmail.com>
2020-06-14 17:31:54 -0400

    Week8 folder created.

---
+++ week09/PokeAPI/pokeapi.js
@@ -0,0 +1,90 @@
+const pBtn = document.querySelector('#pokemonPreBtn');
+const nBtn = document.querySelector('#pokemonNextBtn');
+const sBtn = document.querySelector('#startBtn');
+
+console.log(pBtn);
+console.log(nBtn);
+console.log(sBtn);
+
+async function getPokemon(url) {
+    if (url != "" || null) {
+        try {
+            const response = await fetch(url);
+            if (!response.ok) {
+                throw Error(response.statusText)
+            } else {
+                const fetchJson = await response.json();
+                console.log(fetchJson)
+                renderPokemon(fetchJson)
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+function renderPokemon(data) {
+    sBtn.setAttribute("style", "display:none");
+    //document.getElementByClassName("page-link").style.display="block";
+    const pokemonContainer = document.querySelector('#pokemonList');
+    console.log(pokemonContainer);
+    //button.lastElementChild.remove();  
+    //remove all child nodes
+    while (pokemonContainer.hasChildNodes()) {
+        pokemonContainer.removeChild(pokemonContainer.firstChild);
+    }
+
+    if (data.next || data.previous) {
+        console.log(data.previous);
+        //nBtn.textContent = "Next";
+        //Btn.setAttribute("style", "display:block");
+        //pBtn.setAttribute("class", "btn");
+        if (data.previous == null) {
+            pBtn.setAttribute('ontouchend', `getPokemon('${""}')`);
+            pBtn.setAttribute('onclick', `getPokemon('${""}')`);
+        } else {
+            pBtn.setAttribute('ontouchend', `getPokemon('${data.previous}')`);
+            pBtn.setAttribute('onclick', `getPokemon('${data.previous}')`);
+        }
+        
+
+        nBtn.textContent = "Next";
+        //nBtn.setAttribute("style", "float:right");
+        nBtn.setAttribute('ontouchend', `getPokemon('${data.next}')`);
+        nBtn.setAttribute('onclick', `getPokemon('${data.next}')`);
+    }
+    
+    console.log(data.results);
+    data.results.forEach( pokemon => {
+        const listItem = document.createElement('li');
+        listItem.setAttribute("class", "list-group-item");
+        // listItem.setAttribute("class", "list-group-item");
+        listItem.innerHTML =
+        `
+            <span>${pokemon.name}</span> <button class="btn btn-primary btn-sm" ontouchend="getDetails('${pokemon.name}', '${pokemon.url}')" onclick="getDetails('${pokemon.name}', '${pokemon.url}')">View</button>
+        `
+        listItem.id = pokemon.name;
+        pokemonContainer.appendChild(listItem);
+    })
+}
+
+function getDetails(id, url) {                                  //id=pokemon.name, url=details of pokemon
+    fetch(url)
+        .then( res => res.json() )                              //response a string of array
+        .then( json => {                                        //response promise  (=> a fat arrow syntax)
+            console.log('DETAILS ', json);
+            var li = document.getElementById(id);
+            var div = document.createElement('div');            //create div for image
+            div.innerHTML = 
+            `
+                <img src="${json.sprites.front_default}" />
+                <div>
+                <p>Base Experience: ${json.base_experience}</p>
+                <p>Height: ${json.height}</p>
+                <p>Weight: ${json.weight}</p>
+                </div>
+            `
+            li.lastElementChild.remove();                       //remove button
+            li.appendChild(div);                                //append Child again
+        })
+}
